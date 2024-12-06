890626a06422d3b04c5fcd2d19ed9b42ef5fca89
Kevin Keller2 <kellerkev@gmail.com>
2024-12-06 12:07:15 +0000

    remove url

--- hello_world_flask/startme_from_env.sh
+++ hello_world_flask/startme_from_env.sh
@@ -1,4 +1,4 @@
-cd /home/websson/ && wget https://fourmore-porttest3.hetz-fsn-prod-1.fineupp.com/extraport/hello_world_flask.zip
+cd /home/websson/ && wget https://xxxxxx.fineupp.com/extraport/hello_world_flask.zip
 cd /home/websson/ && unzip hello_world_flask.zip
 cd /home/websson/ && rm hello_world_flask.zip
 cd /home/websson/hello_world_flask && . /home/websson/default_py_env/bin/activate && pip install -r requirements.py
